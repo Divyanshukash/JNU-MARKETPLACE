1e10d2d1068d177c7502af6b52e083f6afa121e4
DIVYANSHU KASHYAP <kashyapdivyanshu002@gmail.com>
2026-06-22 22:29:06 +0530

    Update ListingRequest.java

--- backend/src/main/java/com/jnu/marketplace/dto/ListingRequest.java
+++ backend/src/main/java/com/jnu/marketplace/dto/ListingRequest.java
@@ -13,11 +13,11 @@ import java.util.List;
 public class ListingRequest {
 
     @NotBlank(message = "Title is required")
-    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
+    @Size(min = 3, max = 100, message = "Title must be between 5 and 100 characters")
     private String title;
 
     @NotBlank(message = "Description is required")
-    @Size(min = 10, max = 2000, message = "Description must be between 10 and 2000 characters")
+    @Size(min = 5, max = 2000, message = "Description must be between 10 and 2000 characters")
     private String description;
 
     @NotNull(message = "Price is required")
